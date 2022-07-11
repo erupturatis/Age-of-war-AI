69d5154458b98dbafd82d780d4828f4dfae9fea0
erupturatis <bb.octav@gmail.com>
2022-07-11 12:55:41 +0300

    feat: troop detection

--- AgeOfWarAI/game_vision.py
+++ AgeOfWarAI/game_vision.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
 import pyautogui
+from pytesseract import pytesseract
+from pytesseract import Output
 
 class GameVision(object):
 
     env = None
+    screenshoot = None
 
     def __init__(self) -> None:
         pass
@@ -17,15 +20,16 @@ class GameVision(object):
         return dict[args[0]](args[1:])
 
     def battle(self, *args):
-        img = cv2.imread('AgeOfWarAI/assets/game1.png') # here goes game input
-        template = cv2.imread('AgeOfWarAI/assets/coin.png') # here goes standard coin identifier
+        # game input
+        img = self.screenshoot 
+        # standard coin identifier
+        template = cv2.imread('AgeOfWarAI/assets/coin.png')
         h, w, c = template.shape
         h_img, w_img, c_img = img.shape
         # image dimensions
         method = cv2.TM_SQDIFF_NORMED # best method so far
 
-        img2 = img.copy()
-        result = cv2.matchTemplate(img2, template, method)
+        result = cv2.matchTemplate(img, template, method)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
         location = min_loc
@@ -50,9 +54,6 @@ class GameVision(object):
 
     def scan_xp(self):
         pass
-
-    def scan_player_heath(self):
-        pass
     
     def scan_enemy_health(self):
         pass
@@ -61,7 +62,48 @@ class GameVision(object):
         pass
 
     def scan_enemy_troops(self):
-        pass
+
+        def first_age():
+            #gets clubman and slingers together
+            img = cv2.imread('AgeOfWarAI/assets/game1.png')
+            template = cv2.imread('AgeOfWarAI/assets/enemy/clubman6.png')
+            threshhold = 0.65
+
+            method = cv2.TM_CCOEFF_NORMED # best method so far
+            result = cv2.matchTemplate(img, template, method)
+
+            locations = np.where(result >= threshhold)
+            locations = list(zip(*locations[::-1]))
+
+            if locations:
+                template_w = template.shape[1]
+                template_h = template.shape[0]
+                line_color = (0, 255, 0)
+                # clustering the clone values into 1
+                new_locations = list()
+                locations.sort()
+                for i in range(len(locations)-1):
+                    if abs(locations[i][0] - locations[i+1][0]) < 10 or not(locations[i][1]>500 and locations[i][1]<530) or locations[i][0] < 600 or locations[i][0] > 1550:
+                        continue
+                    else:
+                        new_locations.append(locations[i])
+                    
+                print(len(new_locations))
+                print(len(locations))
+                for loc in new_locations:
+                    print(loc)
+                    top_left = loc
+                    bottom_right = (top_left[0] + template_w, top_left[1] + template_h)
+
+                    cv2.rectangle(img, top_left, bottom_right, line_color)
+                
+                cv2.imshow('Matches', img)
+                cv2.waitKey()
+        
+
+    def scan_player_heath(self):
+        img = self.screenshoot
+        print(img)
 
 
 class WindowManagement(object):
@@ -81,12 +123,16 @@ class WindowManagement(object):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def screenshoot(self):
+    def screenshot(self):
         im = pyautogui.screenshot(region=(0,45,1700,525))
         opencvImage = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+        return opencvImage
         
 
 
 if __name__ == "__main__":
-    obj = WindowManagement()
-    obj.screenshoot()
+    img = cv2.imread("AgeOfWarAI/assets/game2.png")
+    pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    image_data = pytesseract.image_to_data(img, output_type=Output.DICT)
+    print(image_data)
+
--- README.md
+++ README.md
@@ -4,25 +4,37 @@ Age of war AI
 
 inputs:
 
-enemy hp - percent of total health
-my hp - percent of total health
-money - normalized by basic unit cost (money/cost_of_basic_unit)
-xp - can upgrade age or not
-player age - one hot encoded
-can activate ability - 0 to 1 cooldown
-my troops on ground 
-enemy troops on ground 
-where is the current battle taking place(0-100 your base to enemy base)
-number of troops in training 
-number of available cannon slots
-(more specific troop type?)
-array with cannon and cannon age (max 4) - calculated while playing
-
+enemy hp - percent of total health (1 input)
+my hp - percent of total health (1 input)
+money - normalized by basic unit cost (money/cost_of_basic_unit) (1 input)
+xp - can upgrade age or not (1 input)
+player age - one hot encoded (5 inputs)
+can activate ability - 0 to 1 cooldown (1 input)
+my troops on ground (1 input)
+enemy troops on ground type 1 and 2 (2 inputs)
+special troops on ground for enemy and for player,last age (2 inputs)
+where is the current battle taking place(0-1 your base to enemy base) (1 input)
+number of troops in training (1 input)
+number of available cannon slots (1 input)
+array with cannon and cannons age (max 4) (1 input for type since they get progressively stronger and 1 input for cannon age)(2 for each slot)  (8 inputs) 
+8 + 1 + 1 + 1 + 2 + 2 + 1 + 1 + 5 + 1 + 1 + 1 + 1 = 26 inputs
 
 outputs:
 
-create troops tier 1,2,3
-buy cannon slot
-buy cannon tier 1,2,3, first available slot
-sell cannon on certain slot
-evolve next age
+create troops tier 1,2,3 (3 actions)
+create toops tier 4 for space age (1 actions)
+buy cannon slot (1 action)
+buy cannon tier 1,2,3, first available slot (3 actions)
+sell cannon on certain slot (4 actions)
+evolve next age (1 action)
+
+13 actions
+
+Dependencies
+
+numpy
+pytesseract
+open-cv
+
+You can install the archive I used for the game from here:
+
